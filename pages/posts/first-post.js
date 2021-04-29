354c29364edc770ae3d074e8135c67a46489093e
Daron <ulaire06@gmail.com>
2021-04-29 04:35:51 +0300

    Vot tak vot

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MainLayout from '../../components/Layouts/MainLayout';
 
 const FirstPost = () => {
-  return <MainLayout>AHA</MainLayout>;
+  return <MainLayout>Vot tak vot</MainLayout>;
 };
 
 export default FirstPost;
