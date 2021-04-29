fc1a0c66a351ddcb03109225f8fab0b7620cc405
Daron <ulaire06@gmail.com>
2021-04-29 04:15:26 +0300

    Прошёл базовый курс

---
+++ components/Image/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Image from 'next/image';
+const MyImage = () => {
+  return <Image src="/images/deva.png" alt="deva" width={190} height={180} />;
+};
+
+export default MyImage;
---
+++ pages/posts/first-post.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MainLayout from '../../components/Layouts/MainLayout';
+
+const FirstPost = () => {
+  return <MainLayout>AHA</MainLayout>;
+};
+
+export default FirstPost;
